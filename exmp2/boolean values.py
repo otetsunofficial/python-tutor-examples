c2a164cee469d9215004d50b0b41163aff14b3f1
otetsunofficial@gmail.com <machine@pc.437>
2026-02-01 17:04:44 +0500

    2nd pack of python tutor

---
+++ exmp2/boolean values.py
@@ -0,0 +1,9 @@
+value = int(input("enter a 1 for true, a 0 for false"))
+if value == 1:
+    bl = True
+elif value == 0:
+    bl = False 
+if bl:
+    print("succes")
+else:
+    print("error")
---
+++ exmp2/while loops.py
@@ -0,0 +1,3 @@
+s = input("enter exit to break ")
+while s != "exit":
+    s = input("enter exit to break ")
